e10aa7adb8fa3cc2368189487954626541258db0
Jakub Cervinka (Z666660) <jakub.cervinka@zf.com>
2020-05-10 11:44:37 +0200

    base game loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import constants as const
 import logging
+from game import Game
 
 log = logging.getLogger(const.LOGGER_NAME)
 
@@ -9,5 +10,11 @@ log = logging.getLogger(const.LOGGER_NAME)
 if __name__ == '__main__':
     log.info(const.BANNER_START)
 
+    # create the game object
+    g = Game()
+    g.show_start_screen()
+    g.new()
+    g.run()
+    g.show_go_screen()
 
     log.info(const.BANNER_END)
